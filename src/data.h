b3721dcf7c48e4cb96211bdc73b8e3c4acade2a0
KeyboardFire <andy@keyboardfire.com>
2015-10-24 15:15:43 -0500

    Split up main.c into multiple files
    Specifically: data.h, radarWin.{c,h}.
    
    Note that this commit also makes isNull() a #define, to simplify
    linking.

---
+++ src/data.h
@@ -0,0 +1,39 @@
+#ifndef __DATA_H__
+#define __DATA_H__
+
+
+#include <ncurses.h>
+
+
+// utility types
+typedef struct { int x; int y; } XY;
+#define isNull(xy) ((xy).x < 0 || (xy).y < 0)
+typedef enum { UP = 1, RIGHT = 2, DOWN = 4, LEFT = 8 } Direction;
+
+// types for storing global data
+typedef struct {
+    XY xy;
+    Direction dir;
+} Airport;
+
+typedef struct {
+    XY xy;
+    Direction dir;
+    char name;
+    int altitude;
+    // TODO fuel, target, speed, etc.
+} Plane;
+
+typedef struct {
+    // these are arrays terminated by isNull(xy)
+    XY *exits;
+    XY *beacons;
+    Airport *airports;
+    Plane *planes;
+
+    int tickDelay;
+    double newPlaneRate;
+    char nextLetter;
+} AtcsoData;
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,44 +1,11 @@
 #include <stdlib.h>
-#include <ncurses.h>
 #include <time.h>
 
 
-// utility types
-typedef struct { int x; int y; } XY;
-int isNull(XY xy) { return xy.x < 0 || xy.y < 0; }
-typedef enum { UP = 1, RIGHT = 2, DOWN = 4, LEFT = 8 } Direction;
-
-
-// types for storing global data
-typedef struct {
-    XY xy;
-    Direction dir;
-} Airport;
-
-typedef struct {
-    XY xy;
-    Direction dir;
-    char name;
-    int altitude;
-    // TODO fuel, target, speed, etc.
-} Plane;
-
-typedef struct {
-    // these are arrays terminated by isNull(xy)
-    XY *exits;
-    XY *beacons;
-    Airport *airports;
-    Plane *planes;
-
-    int tickDelay;
-    double newPlaneRate;
-    char nextLetter;
-} AtcsoData;
+#include "radarWin.h"
 
 
 void mainloop();
-WINDOW *createRadarWin(AtcsoData *data);
-bool updateRadarWin(AtcsoData *data, WINDOW *radarWin);
 
 
 /**
@@ -123,108 +90,4 @@ void mainloop() {
     cleanup:
     free(data.exits);
     delwin(radarWin);
-}
-
-/**
- * Creates the radar window, the biggest one that has all the planes and stuff.
- */
-WINDOW *createRadarWin(AtcsoData *data) {
-    WINDOW *radarWin = newwin(21, 60, 0, 0);
-
-    // draw the outline
-    for (int i = 0; i < 59; ++i) waddch(radarWin, '-');
-    waddch(radarWin, ' ');
-    for (int i = 0; i < 19; ++i) {
-        waddstr(radarWin, "| ");
-        for (int j = 0; j < 28; ++j) waddstr(radarWin, ". ");
-        waddstr(radarWin, "| ");
-    }
-    for (int i = 0; i < 59; ++i) waddch(radarWin, '-');
-    waddch(radarWin, ' ');
-
-    // add the exits
-    XY *xy = data->exits;
-    for (int i = 0; !isNull(*xy); ++xy, ++i) {
-        mvwaddch(radarWin, xy->y, 2 * xy->x, '0' + i);
-    }
-
-    // add the beacons
-    xy = data->beacons;
-    for (int i = 0; !isNull(*xy); ++xy, ++i) {
-        mvwaddch(radarWin, xy->y, 2 * xy->x, '*');
-        waddch(radarWin, '0' + i);
-    }
-
-    // add the airports
-    Airport *airport = data->airports;
-    for (int i = 0; !isNull(airport->xy); ++airport, ++i) {
-        mvwaddch(radarWin, airport->xy.y, 2 * airport->xy.x,
-                "_^>_v___<"[airport->dir]);
-        waddch(radarWin, '0' + i);
-    }
-
-    updateRadarWin(data, radarWin);
-
-    return radarWin;
-}
-
-/**
- * Update and refresh the radar window.
- */
-bool updateRadarWin(AtcsoData *data, WINDOW *radarWin) {
-    int nPlanes = 0;
-    for (Plane *p = data->planes; !isNull(p->xy); ++p, ++nPlanes) {
-        mvwaddstr(radarWin, p->xy.y, 2 * p->xy.x, ". ");
-        // TODO check for beacons, airports; redraw and do actions
-
-        if (p->dir & UP) --p->xy.y;
-        if (p->dir & RIGHT) ++p->xy.x;
-        if (p->dir & DOWN) ++p->xy.y;
-        if (p->dir & LEFT) --p->xy.x;
-
-        if (p->xy.y == 0 || p->xy.y == 20 || p->xy.x == 0 || p->xy.x == 29) {
-            // the plane either exited or crashed
-            if (p->altitude != 9) return true;  // game over
-            for (XY *exit = data->exits; !isNull(*exit); ++exit) {
-                if (exit->y == p->xy.y && exit->x == p->xy.x) {
-                    // woohoo, plane exited!
-                    // TODO make sure it went in the right exit
-                    // TODO remove plane
-                }
-            }
-            return true;  // plane didn't hit an exit; game over
-        }
-
-        mvwaddch(radarWin, p->xy.y, 2 * p->xy.x, p->name);
-        waddch(radarWin, '7');
-    }
-
-    if (rand() < RAND_MAX * data->newPlaneRate) {
-        int nExits = 0;
-        for (XY *exit = data->exits; !isNull(*exit); ++exit, ++nExits);
-
-        XY entryCoords = data->exits[rand() % nExits];
-        Direction entryDir = 0;
-        if (entryCoords.y == 0)  { entryCoords.y = 1;  entryDir |= DOWN;  }
-        if (entryCoords.y == 20) { entryCoords.y = 19; entryDir |= UP;    }
-        if (entryCoords.x == 0)  { entryCoords.x = 1;  entryDir |= RIGHT; }
-        if (entryCoords.x == 29) { entryCoords.x = 28; entryDir |= LEFT;  }
-
-        data->planes = realloc(data->planes, (nPlanes + 2) * sizeof(Plane));
-        data->planes[nPlanes] = (Plane) {entryCoords, entryDir,
-                data->nextLetter, 7};
-        data->planes[nPlanes + 1] = (Plane) {{-1, -1}, 0, 0, 0};
-
-        ++data->nextLetter;
-        if (data->nextLetter > 'Z') data->nextLetter = 'A';
-
-        mvwaddch(radarWin, data->planes[nPlanes].xy.y,
-                2 * data->planes[nPlanes].xy.x,
-                data->planes[nPlanes].name);
-        waddch(radarWin, '0' + data->planes[nPlanes].altitude);
-    }
-
-    wrefresh(radarWin);
-
-    return false;
 }
