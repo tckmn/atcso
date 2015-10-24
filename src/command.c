25d337937361930e63a2bb1a2f10bcd7bdf70539
KeyboardFire <andy@keyboardfire.com>
2015-10-24 18:35:03 -0500

    Move mk*tn() to .c file, not .h
    They don't really belong there, and nobody else needs them.

--- src/command.c
+++ src/command.c
@@ -15,6 +15,10 @@ void altitudeSet(AtcsoData *data, char plane, char extra) {
     // TODO
 }
 
+TreeNode *mk1tn(TreeNode n1);
+TreeNode *mk2tn(TreeNode n1, TreeNode n2);
+TreeNode *mk3tn(TreeNode n1, TreeNode n2, TreeNode n3);
+
 void initializeCommands() {
     commands = (TreeNode) {0, "", NULL, mk2tn(
         (TreeNode) {'a', "altitude", NULL, mk3tn(
@@ -28,4 +32,25 @@ void initializeCommands() {
         ), 0},
         (TreeNode) {'t', "turn", NULL, NULL, 0}
     ), 2};
+}
+
+TreeNode *mk1tn(TreeNode n1) {
+    TreeNode *a = malloc(1 * sizeof(TreeNode));
+    a[0] = n1;
+    return a;
+}
+
+TreeNode *mk2tn(TreeNode n1, TreeNode n2) {
+    TreeNode *a = malloc(2 * sizeof(TreeNode));
+    a[0] = n1;
+    a[1] = n2;
+    return a;
+}
+
+TreeNode *mk3tn(TreeNode n1, TreeNode n2, TreeNode n3) {
+    TreeNode *a = malloc(3 * sizeof(TreeNode));
+    a[0] = n1;
+    a[1] = n2;
+    a[2] = n3;
+    return a;
 }
--- src/command.h
+++ src/command.h
@@ -3,7 +3,6 @@
 
 #include "data.h"
 
-
 struct treeNode_t;
 typedef struct treeNode_t {
     char hotkey;
@@ -15,26 +14,5 @@ typedef struct treeNode_t {
 
 extern TreeNode commands;
 void initializeCommands();
-
-TreeNode *mk1tn(TreeNode n1) {
-    TreeNode *a = malloc(1 * sizeof(TreeNode));
-    a[0] = n1;
-    return a;
-}
-
-TreeNode *mk2tn(TreeNode n1, TreeNode n2) {
-    TreeNode *a = malloc(2 * sizeof(TreeNode));
-    a[0] = n1;
-    a[1] = n2;
-    return a;
-}
-
-TreeNode *mk3tn(TreeNode n1, TreeNode n2, TreeNode n3) {
-    TreeNode *a = malloc(3 * sizeof(TreeNode));
-    a[0] = n1;
-    a[1] = n2;
-    a[2] = n3;
-    return a;
-}
 
 #endif
